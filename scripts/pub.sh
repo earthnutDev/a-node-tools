0bab8355ec30974b534e429ec21bc7f3c7cdb593
earth nut dev <earthnut.dev@outlook.com>
2025-08-01 09:55:38 +0800

    submit :  2025/8/1 09:55:38

--- scripts/pub.sh
+++ scripts/pub.sh
@@ -8,8 +8,8 @@ REPO_ROOT=$REPO_ROOT
 PUB_ERROR=()
 CHECK_VERSION="@qqi/check-version"
 # 读取版本检测是否可用
-# source ./scripts/check_version_install.sh 
-printf $(pnpm dlx "${CHECK_VERSION}" -v)  # 更改全局安装的测试方法
+source ./scripts/check_version_install.sh 
+
 echo "工作根路径 $REPO_ROOT"
 PACKAGES_DIR="${REPO_ROOT}/packages"
 # 将字符串转为数组
@@ -22,7 +22,7 @@ update_version() {
 
     local tag=""
     cd $REPO_ROOT # 每次需要手动更新到根下才能正确的校验版本号
-    if ! tag=$(pnpm dlx "${CHECK_VERSION}" n=${input} 2>&1); then
+    if ! tag=$(npx "${CHECK_VERSION}" n=${input} 2>&1); then
        echo "未通过版本校验：$tag"
        return 0 
     fi
@@ -66,8 +66,8 @@ main() {
       exit 0
     fi
     echo "☁️ 来"
-    # install_check_version # 检查版本包安装校验
-    printf $(pnpm dlx "${CHECK_VERSION}" -v)  # 更改全局安装的测试方法
+    install_check_version # 检查版本包安装校验
+    printf $(npx "${CHECK_VERSION}" -v)  # 更改全局安装的测试方法
     # 遍历变更的包数组，进行 npm 包推送
     # "${ARR[@]}" 引用数组所有元素
     # "${!ARR[@]}" 引用数组所有索引 ${ARR[$index]}
